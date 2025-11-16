0b92f6347df5d7ae862efb4c2a26041ac466c735
ct <851320384@qq.com>
2025-11-16 20:14:16 +0800

    新增导出

---
+++ src/dplib/core/data/__init__.py
@@ -0,0 +1,89 @@
+"""Core data abstractions shared across NSSDPlib."""
+
+from .domain import (
+    BaseDomain,
+    DomainError,
+    DomainInfo,
+    DiscreteDomain,
+    ContinuousDomain,
+    BucketizedDomain,
+)
+from .dataset import (
+    Dataset, 
+    DatasetError, 
+    DatasetMetadata,
+    DataRecord,
+)
+from .transformers import (
+    DataTransformer,
+    ClippingTransformer,
+    NormalizationTransformer,
+    DiscretizerTransformer,
+    OneHotEncoder,
+    TransformerPipeline,
+    TransformationError,
+)
+from .validation import (
+    Schema,
+    SchemaField,
+    SchemaValidator,
+    ValidationStrategy,
+    DataValidationError,
+    detect_missing,
+)
+from .statistics import (
+    count, 
+    summation, 
+    mean, 
+    variance, 
+    histogram, 
+    RunningStats,
+)
+from .sensitivity import (
+    SensitivityError,
+    count_global_sensitivity,
+    sum_global_sensitivity,
+    mean_global_sensitivity,
+    local_sensitivity,
+    SmoothSensitivityEstimate,
+    smooth_sensitivity_mean,
+)
+
+__all__ = [
+    "BaseDomain",
+    "DomainError",
+    "DomainInfo",
+    "DiscreteDomain",
+    "ContinuousDomain",
+    "BucketizedDomain",
+    "Dataset",
+    "DatasetError",
+    "DatasetMetadata",
+    "DataRecord",
+    "DataTransformer",
+    "ClippingTransformer",
+    "NormalizationTransformer",
+    "DiscretizerTransformer",
+    "OneHotEncoder",
+    "TransformerPipeline",
+    "TransformationError",
+    "Schema",
+    "SchemaField",
+    "SchemaValidator",
+    "ValidationStrategy",
+    "DataValidationError",
+    "detect_missing",
+    "count",
+    "summation",
+    "mean",
+    "variance",
+    "histogram",
+    "RunningStats",
+    "SensitivityError",
+    "count_global_sensitivity",
+    "sum_global_sensitivity",
+    "mean_global_sensitivity",
+    "local_sensitivity",
+    "SmoothSensitivityEstimate",
+    "smooth_sensitivity_mean",
+]
