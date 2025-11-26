5cf2d999b2e508fc8b962a058454b75f284fefa0
ct <851320384@qq.com>
2025-11-26 16:31:06 +0800

    cdp: 在机制包 init 中导出指数、几何、阶梯及向量等新机制

--- src/dplib/cdp/mechanisms/__init__.py
+++ src/dplib/cdp/mechanisms/__init__.py
@@ -1,5 +1,16 @@
 """Centralised differential privacy mechanisms."""
 from .laplace import LaplaceMechanism
 from .gaussian import GaussianMechanism
+from .exponential import ExponentialMechanism
+from .geometric import GeometricMechanism
+from .staircase import StaircaseMechanism
+from .vector import VectorMechanism
 
-__all__ = ["LaplaceMechanism", "GaussianMechanism"]
+__all__ = [
+    "LaplaceMechanism",
+    "GaussianMechanism",
+    "ExponentialMechanism",
+    "GeometricMechanism",
+    "StaircaseMechanism",
+    "VectorMechanism",
+]
