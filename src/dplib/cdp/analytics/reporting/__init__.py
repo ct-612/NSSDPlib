9f7cbc2a90ceb0f7984ab9700da41e82b4a875f3
ct <851320384@qq.com>
2025-12-07 17:15:55 +0800

    cdp: 导出隐私与效用报告模型的公共接口

---
+++ src/dplib/cdp/analytics/reporting/__init__.py
@@ -0,0 +1,23 @@
+from .privacy_report import (
+    PrivacyAnnotation,
+    PrivacyBudgetSnapshot,
+    PrivacyReport,
+    PrivacyUsageRecord,
+)
+from .utility_report import (
+    ErrorMetrics,
+    QueryUtilityRecord,
+    UtilityCurve,
+    UtilityReport,
+)
+
+__all__ = [
+    "PrivacyAnnotation",
+    "PrivacyBudgetSnapshot",
+    "PrivacyReport",
+    "PrivacyUsageRecord",
+    "ErrorMetrics",
+    "QueryUtilityRecord",
+    "UtilityCurve",
+    "UtilityReport",
+]
